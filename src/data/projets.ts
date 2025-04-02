7ca331d5b968b55b65d6ef2958d618fd6e4e126e
kayembajoel92@gmail.com <kayembajoel92@gmail.com>
2025-04-02 09:18:32 -0400

    add new project

--- src/data/projets.ts
+++ src/data/projets.ts
@@ -1,27 +1,34 @@
 export const projets = [
     {
       id: 1,
+      titre: "visite-RDC",
+      description: "Un site web qui partage l'histoire et la culture de mon pays la Rdc",
+      image: "/assets/projets/visite-rdc.png",
+      lien: "https://visite-rdc.netlify.app/"
+    },
+    {
+      id: 2,
       titre: "CineVault",
       description: "Une application web qui permet de rechercher des films et séries, consulter les descriptions, et obtenir des liens de streaming gratuits avec une interface moderne et responsive.",
       image: "/assets/projets/cinevault.jpg",
       lien: "https://github.com/JoelKayemba/CineVault",
     },
     {
-      id: 2,
+      id: 3,
       titre: "Mon-Entraînement",
       description: "Une application mobile React Native pour créer et gérer des entraînements personnalisés, avec minuteurs, vidéos explicatives, et gestion des temps de repos selon ses propres critères.",
       image: "/assets/projets/mon-entrainement.jpg",
       lien: "https://github.com/JoelKayemba/mon-entrainement2",
     },
     {
-      id: 3,
+      id: 4,
       titre: "MECAZEN",
       description: "Application web de gestion des services automobiles développée avec React et Redux, permettant la gestion des véhicules, rendez-vous et historique des réparations sans backend.",
       image: "/assets/projets/mecazen.jpg",
       lien: "https://github.com/JoelKayemba/Mecazen",
     },
     {
-      id: 4,
+      id: 5,
       titre: "Password Secure",
       description: "Une extension de navigateur pour générer des mots de passe uniques et sécurisés pour chaque site, sans stockage, basée sur une clé utilisateur, des questions/réponses et un token par site.",
       image: "/assets/projets/secure_password.png",
